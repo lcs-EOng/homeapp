5eb1825f22b3728b986cde958c66910b2e627fba
Eason Ong <eong@lcs.on.ca>
2024-01-11 14:06:36 -0500

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by Ong Eason on 11/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
